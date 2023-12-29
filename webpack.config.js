78e5f52a0a4779b594e6e72a83a1d1a832a053c6
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-29 16:40:36 -0500

    My first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo",
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
